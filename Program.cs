b3fc4ccb36171a655f7247154115ea1ee5183557
Matheus Farias <mf0560272@gmail.com>
2024-12-20 18:10:21 -0300

    Uso de if/else e switch/case

--- Program.cs
+++ Program.cs
@@ -14,6 +14,24 @@ void OpcoesDoMenu ()
     Console.WriteLine("Digite 3 para avaliar uma banda \n");
     Console.WriteLine("Digite 4 para exibir a media \n"); 
     Console.WriteLine("Digite 5 para sair \n");
+    Console.Write("Digite a opção que dele selecionar: "); 
+    string opcaoEscolhida =     Console.ReadLine()!;
+    int opcaoEscolhidaNuemerica = int.Parse(opcaoEscolhida);
+    
+    switch(opcaoEscolhidaNuemerica){
+        case 1: Console.WriteLine("Opção escolhida: " + opcaoEscolhidaNuemerica); 
+            break; 
+        case 2: Console.WriteLine("Opção escolhida: " + opcaoEscolhidaNuemerica); 
+            break; 
+        case 3: Console.WriteLine("Opção escolhida: " + opcaoEscolhidaNuemerica); 
+            break; 
+        case 4: Console.WriteLine("Opção escolhida: " + opcaoEscolhidaNuemerica); 
+            break; 
+        case 5: Console.WriteLine("Opção escolhida: " + opcaoEscolhidaNuemerica); 
+            break; 
+        default: Console.WriteLine("Opção Invalida"); 
+            break; 
+    }
 }
 
 OpcoesDoMenu(); 
